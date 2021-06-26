526ba0d18ed05a298e743c5c714e7e87a34d59de
浮云 <huangbo@cai-inc.com>
2021-06-26 19:54:43 +0800

    feat：新增订阅功能

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,6 +10,7 @@ export default class Header extends Component {
           <span className="router-links">
             <a href="/">首页</a>
             <a href="/summary">数据看板</a>
+            <a href="http://localhost:1200/zooTeam/weekly" target="_blank" rel="noreferrer"><img src="../../../static/images/rss.png" alt="rss" className="rss" /></a>
           </span>
         </h1>
       </header>
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // 后端服务的ip和端口。3030在后端项目中写死，可以到后端项目中修改
-const BASE_URL = 'http://ip:3030';
+const BASE_URL = 'http://localhost:3030';
 const instance = axios.create({
   baseURL: BASE_URL,
 });
